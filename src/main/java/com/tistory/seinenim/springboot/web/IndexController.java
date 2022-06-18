0842b46b63bcba835f6227e4d44649c871591549
WonjunChun <chunjh1103@naver.com>
2022-06-18 20:27:28 +0900

    게시글 등록 화면 추가

--- src/main/java/com/tistory/seinenim/springboot/web/IndexController.java
+++ src/main/java/com/tistory/seinenim/springboot/web/IndexController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class IndexController { //페이지에 관련된 컨트롤러는 모두 IndexController 사용
     @GetMapping("/")
     public String index(){
         //mustache 스타터 덕분에 문자열 반환시 앞의 경로와 뒤의 파일 확장자 자동 지정됨
         // src/main/resources/templates/index.mustache
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
